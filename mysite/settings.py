915068d21d0e2202b0938f77bb1489ae9a8a34f3
N.T <b1017158@fun.ac.jp>
2018-10-29 17:28:37 +0900

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,3 +135,8 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
